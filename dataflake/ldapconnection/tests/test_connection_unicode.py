3cbaf5d5bb4bbfae866413088075361ff675833d
Stefan H. Holek <stefan@epy.co.at>
2012-03-17 23:06:02 +0100

    Add tests for Unicode support.

---
+++ dataflake/ldapconnection/tests/test_connection_unicode.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2008-2012 Jens Vagelpohl and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" test_connection_unicode: Tests for the LDAPConnection Unicode support
+
+$Id:$
+"""
+
+import unittest
+
+from dataflake.ldapconnection.tests.base import LDAPConnectionTests
+
+
+class UnicodeSupportTests(LDAPConnectionTests):
+
+    def test_search_unicode_results(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': 'Bjørn'}
+        conn.insert('dc=localhost', 'cn=foo', attrs=attrs)
+
+        response = conn.search('dc=localhost', fltr='(cn=foo)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=foo,dc=localhost'
+                          , 'cn': [u'foo']
+                          , 'displayName': [u'Bjørn']
+                          }
+                        )
+
+    def test_search_raw_results(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': 'Bjørn'}
+        conn.insert('dc=localhost', 'cn=foo', attrs=attrs)
+
+        response = conn.search('dc=localhost', fltr='(cn=foo)', raw=True)
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': 'cn=foo,dc=localhost'
+                          , 'cn': ['foo']
+                          , 'displayName': ['Bjørn']
+                          }
+                        )
+
+    def test_insert_unicode_data(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': u'Bjørn'}
+        conn.insert(u'dc=localhost', u'cn=føø', attrs=attrs)
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=føø)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=føø,dc=localhost'
+                          , 'cn': [u'føø']
+                          , 'displayName': [u'Bjørn']
+                          }
+                        )
+
+    def test_modify_unicode_data(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': u'Bjørn'}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+        conn.modify(u'cn=foo,dc=localhost', attrs={'displayName': u'Bjørn Åge'})
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=foo)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=foo,dc=localhost'
+                          , 'cn': [u'foo']
+                          , 'displayName': [u'Bjørn Åge']
+                          }
+                        )
+
+    def test_modify_multivalued_unicode_data(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'cn': [u'foo', u'Bjørn Åge']}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+        conn.modify(u'cn=foo,dc=localhost', attrs={'cn': [u'foo', u'Bjørn', u'Bjørn Åge']})
+        conn.modify(u'cn=foo,dc=localhost', attrs={'cn': [u'foo', u'Bjørn']})
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=foo)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=foo,dc=localhost'
+                          , 'cn': [u'foo', u'Bjørn']
+                          }
+                        )
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=Bjørn)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=foo,dc=localhost'
+                          , 'cn': [u'foo', u'Bjørn']
+                          }
+                        )
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=Bjørn Åge)')
+        self.assertEqual(response['size'], 0)
+
+    def test_modify_unicode_rdn(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': u'Bjørn'}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+        conn.modify(u'cn=foo,dc=localhost', attrs={'cn': u'bar'})
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=bar)')
+        self.assertEqual(response['size'], 1)
+
+        results = response['results']
+        self.assertEqual( results[0]
+                        , { 'dn': u'cn=bar,dc=localhost'
+                          , 'cn': [u'bar']
+                          , 'displayName': [u'Bjørn']
+                          }
+                        )
+
+    def test_delete_by_unicode_dn(self):
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'displayName': u'Bjørn'}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+        conn.delete(u'cn=foo,dc=localhost')
+
+        response = conn.search(u'dc=localhost', fltr=u'(cn=foo)')
+        self.assertEqual(response['size'], 0)
+
+    def test_bind_with_valid_unicode_credentials(self):
+        from dataflake.ldapconnection.tests import fakeldap
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'userPassword': fakeldap.hash_pwd('secret')}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+
+        response = conn.search( u'dc=localhost'
+                              , fltr=u'(cn=foo)'
+                              , bind_dn=u'cn=foo,dc=localhost'
+                              , bind_pwd=u'secret'
+                              )
+        self.assertEqual(response['size'], 1)
+
+    def test_bind_with_invalid_unicode_credentials(self):
+        import ldap
+        from dataflake.ldapconnection.tests import fakeldap
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'userPassword': fakeldap.hash_pwd('secret')}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+
+        self.assertRaises( ldap.INVALID_CREDENTIALS
+                         , conn.search
+                         , u'dc=localhost'
+                         , fltr=u'(cn=foo)'
+                         , bind_dn=u'cn=foo,dc=localhost'
+                         , bind_pwd=u'geheim'
+                         )
+
+    def test_bind_with_valid_unicode_credentials_from_connection(self):
+        from dataflake.ldapconnection.tests import fakeldap
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'userPassword': fakeldap.hash_pwd('secret')}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+
+        conn.bind_dn = u'cn=foo,dc=localhost'
+        conn.bind_pwd = u'secret'
+        response = conn.search( u'dc=localhost'
+                              , fltr=u'(cn=foo)'
+                              )
+        self.assertEqual(response['size'], 1)
+
+    def test_bind_with_invalid_unicode_credentials_from_connection(self):
+        import ldap
+        from dataflake.ldapconnection.tests import fakeldap
+        conn = self._makeSimple()
+        conn.api_encoding = None
+
+        attrs = {'userPassword': fakeldap.hash_pwd('secret')}
+        conn.insert(u'dc=localhost', u'cn=foo', attrs=attrs)
+
+        conn.bind_dn = u'cn=foo,dc=localhost'
+        conn.bind_pwd = u'geheim'
+        self.assertRaises( ldap.INVALID_CREDENTIALS
+                         , conn.search
+                         , u'dc=localhost'
+                         , fltr=u'(cn=foo)'
+                         )
+
+
+def test_suite():
+    import sys
+    return unittest.findTestCases(sys.modules[__name__])
+
